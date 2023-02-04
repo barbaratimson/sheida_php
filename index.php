5f196adc789fb3d11e9a8f02c873ca6920146f2c
barbaratimson <cokolek.sokol2014@mail.ru>
2023-02-05 02:31:00 +0700

    19

--- index.php
+++ index.php
@@ -100,5 +100,102 @@
       echo(substr('fsfdfsdfsfdabcd',-3,3));
     ?>
     <br></br>
+
+    <div>Задание 11</div>
+    <?php
+    $a  = substr('http://code.mu/ru/php/book/prime/inbuilt/string/', 0, 7);
+    if ($a == 'http://') {
+        echo('http');
+    } else {
+        echo('Not a http');
+    }
+    ?>
+    <br></br>
+
+    <div>Задание 12</div>
+    <?php
+    $c = 'https://code.mu/ru/php/book/prime/inbuilt/string/';
+    $a  = substr($c, 0, 7);
+    $b = substr($c, 0, 8);
+    if ($a == 'http://') {
+        echo('http');
+    } 
+    else if ($b == 'https://') {
+        echo('https');
+    }
+    else {
+        echo('Not a http/https');
+    }
+    ?>
+    <br></br>
+
+    <div>Задание 13</div>
+    <?php
+    $a  = substr('aboba.png', -4, 4);
+    if ($a == '.png') {
+        echo ('png');
+    } else {
+        echo('Not a png');
+    }
+    ?>
+    <br></br>
+
+    <div>Задание 15</div>
+    <?php
+    $c = 'aboboba.jpg';
+    $a  = substr($c, -4, 4);
+    if ($a == '.png') {
+        echo('png');
+    } 
+    else if ($a == '.jpg') {
+        echo('jpg');
+    }
+    else {
+        echo('Не png/jpg');
+    }
+    ?>
+    <br></br>
+
+    <div>Задание 16</div>
+    <?php
+    $a = 'DeserializeUnableToConvertValue';
+    if (strlen($a) > 5) {
+       $a = substr($a,0,5);
+        echo ("{$a}...");
+    } else if (strlen($a) <= 5) {
+        echo ($a);
+    }
+    ?>
+    <br></br>
+
+    <div>Задание 17</div>
+    <?php
+    echo (str_replace('.', '-', '31.12.2013'));
+    ?>
+    <br></br>
+
+    <div>Задание 18</div>
+    <?php
+    $a = 'agsdgbgsdgbgsdsbcsdgscgsdgc';
+    $a = str_replace('a', '1', $a);
+    $a = str_replace('b', '2', $a);
+    $a = str_replace('c', '3', $a);
+    echo ($a);
+    ?>
+    <br></br>
+
+    <div>Задание 19</div>
+    <?php
+    $a = '1a2b3c4b5d6e7f8g9h0';
+    $b = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    foreach ($b as $elem) {
+        $a = str_replace($elem, '', $a);
+    }
+
+    echo ($a);
+    ?>
+    <br></br>
+
+
 </body>
 </html>
